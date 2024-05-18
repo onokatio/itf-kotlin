d2bfa07819e633f17064105313857743c1a0d871
onokatio <onokatio@gmail.com>
2024-05-18 16:59:33 +0900

    init sample

---
+++ LispInterpreter.kt
@@ -0,0 +1,181 @@
+package com.example.lispinterpreter
+
+fun main() {
+    // Example of usage of the interpreter.
+    val interpreter = LispInterpreter()
+    interpreter.interpret("12")
+    interpreter.interpret("()")
+    interpreter.interpret("((1 2) ((2) 3))")
+}
+
+/**
+ * LISP interpreter implementation.
+ * To run interpretation, give a LISP code string to [interpret].
+ *
+ * Roughly speaking, the interpretation consists of three steps:
+ * 1. [tokenize]: Split LISP text for each tokens.
+ *   e.g., "(1 (2 3))" -> ["(", "1", "(", "2", "3", ")", ")"]
+ * 2. [parse]: Group tokens by parentheses.
+ *   e.g., ["(", "1", "(", "2", "3", ")", ")"] -> ["1", ["2", "3"]]
+ * 3. `evaluate`: (not implemented yet)
+ */
+class LispInterpreter {
+
+    private val state = ParseState("", 0)
+
+    fun interpret(lispText: String) {
+        state.lispText = lispText
+        state.index = 0
+        val tokens = tokenize()
+        while (state.index < tokens.size) {
+            val parseResult = parse(tokens)
+
+            // TODO: Implement `evaluation` logic. Currently, just show the parse result.
+            println(parseResult)
+        }
+    }
+
+    private fun tokenize(): List<String> {
+        val text = state.lispText
+        val tokens: MutableList<String> = mutableListOf()
+        var startIndex = 0
+        var endIndex = 0
+
+        while (endIndex < text.length) {
+            val char = text[endIndex]
+            if (!Symbols.singleCharToken(char)) {
+                endIndex += 1
+                continue
+            }
+
+            if (startIndex < endIndex) {
+                tokens += text.substring(startIndex, endIndex)
+            }
+            if (char != Symbols.SPACE) {
+                tokens += char.toString()
+            }
+            startIndex = endIndex + 1
+            endIndex += 1
+        }
+
+        if (startIndex < text.length) {
+            tokens += text.substring(startIndex)
+        }
+        return tokens
+    }
+
+    private fun parse(tokens: List<String>): ParseResult {
+        if (state.index >= tokens.size) {
+            return ParseResult(null, SyntaxError.UnexpectedEos)
+        }
+        val tokenString = tokens[state.index]
+        if (tokenString == Symbols.S_CLOSE) {
+            return ParseResult(null, SyntaxError.UnexpectedToken(tokenString))
+        }
+
+        return when {
+            tokenString != Symbols.S_OPEN -> {
+                state.index += 1
+                ParseResult(ParsedTokens.SingleToken(tokenString), null)
+            }
+
+            tokens[state.index + 1] == Symbols.S_CLOSE -> {
+                state.index += 2
+                ParseResult(ParsedTokens.TokenGroup(listOf()), null)
+            }
+
+            else -> {
+                state.index += 1
+                val parseResult = parse(tokens)
+                if (parseResult.tokens != null && parseResult.error == null) {
+                    parseTokensInParentheses(parseResult.tokens, tokens)
+                } else {
+                    parseResult
+                }
+            }
+        }
+    }
+
+    private fun parseTokensInParentheses(
+        tokens: ParsedTokens,
+        tokens0: List<String>
+    ): ParseResult {
+        val tokensInParentheses: MutableList<ParsedTokens> = mutableListOf(tokens)
+        var indexInParentheses: Int = state.index
+
+        while (tokens0[indexInParentheses] != Symbols.S_CLOSE) {
+            if (indexInParentheses >= tokens0.size) {
+                return ParseResult(null, SyntaxError.UnexpectedEos)
+            }
+
+            state.index = indexInParentheses
+            val recursiveResult = parse(tokens0)
+            if (recursiveResult.tokens == null || recursiveResult.error != null) {
+                return recursiveResult
+            }
+
+            indexInParentheses = state.index
+            tokensInParentheses.add(recursiveResult.tokens)
+        }
+        state.index = indexInParentheses + 1
+        return ParseResult(ParsedTokens.TokenGroup(tokensInParentheses), null)
+    }
+}
+
+/**
+ * Definition of special characters and the corresponding strings:
+ * the token separator [SPACE] and parenthesis symbols.
+ */
+object Symbols {
+    const val SPACE = ' '
+    private const val OPEN_PARENTHESIS = '('
+    private const val CLOSE_PARENTHESIS = ')'
+
+    private val SYMBOL_CHAR_SET: Set<Char> = setOf(SPACE, OPEN_PARENTHESIS, CLOSE_PARENTHESIS)
+
+    const val S_OPEN = OPEN_PARENTHESIS.toString()
+    const val S_CLOSE = CLOSE_PARENTHESIS.toString()
+
+    fun singleCharToken(char: Char): Boolean = SYMBOL_CHAR_SET.contains(char)
+}
+
+/**
+ * Result model of parsing a token list
+ *
+ * This structure consists of a single token type and a token group type.
+ * A token group has models of the same type recursively.
+ */
+sealed interface ParsedTokens {
+    class TokenGroup(val tokens: List<ParsedTokens>) : ParsedTokens {
+        override fun toString(): String =
+            tokens.joinToString(separator = ", ", prefix = "[", postfix = "]")
+    }
+
+    class SingleToken(val token: String) : ParsedTokens {
+        override fun toString(): String = token
+    }
+}
+
+sealed interface SyntaxError {
+    object UnexpectedEos : SyntaxError
+    class UnexpectedToken(val token: String) : SyntaxError
+}
+
+/**
+ * Intermediate state of interpretation.
+ *
+ * The properties are expected to change during tokenizing and parsing.
+ */
+class ParseState(
+    /** Raw input text as LISP code */
+    var lispText: String,
+    /** Index representing a token position to be parsed */
+    var index: Int,
+)
+
+/**
+ * [tokens] or [error] is non-null exclusively.
+ */
+class ParseResult(val tokens: ParsedTokens?, val error: SyntaxError?) {
+    override fun toString(): String = tokens?.toString() ?: error?.toString() ?: ""
+}
